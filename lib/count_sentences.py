c105384e5c4f500e4430895a4162b0e5f730248c
Lactor <lactorjm4@gmail.com>
2023-10-20 20:21:43 +0300

    Test pending

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,20 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('The value must be a string.')
+
+    def is_question(self):
+        return self.value.endswith('The value must be a string?')
+
+    def is_exclamation(self):
+        return self.value.endswith('THe value must be a string!')
+
+    def count_sentences(self):
+        import re
+        sentences = re.split(r'[.!?]+', self.value)
+        non_empty_sentences = list(filter(lambda x: x.strip(), sentences))
+        return len(non_empty_sentences)
